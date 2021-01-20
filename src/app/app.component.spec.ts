c83dc91d0c3e88daf1dcfe21c362be5ec821750e
axelkr <axelkr.dev@gmail.com>
2021-01-20 04:54:03 +0100

    clean: no need to keep this around

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,11 +29,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-/*
-  it(`should have as title 'EximiousBarnacle'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('EximiousBarnacle');
-  });
-*/
 });
