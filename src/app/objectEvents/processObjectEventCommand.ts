3f11617ffe52a2d8e77cc4914f863e7073d4c8b0
Axel <axelkr.dev@google.com>
2021-01-26 20:41:33 +0100

    moved into external library

--- src/app/objectEvents/processObjectEventCommand.ts
+++
@@ -1,7 +0,0 @@
-import { ObjectEvent } from './objectEvent';
-import { Task } from '../model/task';
-
-export interface ProcessObjectEventCommand {
-  readonly objectEventTypeProcessing: string;
-  process(objectEvent: ObjectEvent, tasks: Task[]): Task[];
-}
