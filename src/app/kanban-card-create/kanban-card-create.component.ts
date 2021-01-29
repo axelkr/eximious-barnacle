7ada5d6206d4afa43a45be6a0120c3f2c0a9f44a
Axel <axelkr.dev@google.com>
2021-01-29 05:22:47 +0100

    add new card to initial state

--- src/app/kanban-card-create/kanban-card-create.component.ts
+++ src/app/kanban-card-create/kanban-card-create.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { State } from 'outstanding-barnacle';
 import { HeijunkaBoardService } from '../heijunka-board.service';
 
 @Component({
@@ -7,6 +8,7 @@ import { HeijunkaBoardService } from '../heijunka-board.service';
   styleUrls: ['./kanban-card-create.component.less']
 })
 export class KanbanCardCreateComponent implements OnInit {
+  @Input() project: string | undefined;
   model = { name: '' };
 
   constructor(private modelBoardService: HeijunkaBoardService) { }
@@ -17,10 +19,29 @@ export class KanbanCardCreateComponent implements OnInit {
   onSubmit() { }
 
   newKanbanCard(): void {
-    const project = this.modelBoardService.getHeijunkaBoard().projects[0];
-    const objectEvent = this.modelBoardService.eventFactory.createKanbanCard(this.modelBoardService.currentTopic,
-      project, this.model.name);
-    this.modelBoardService.processObjectEvent(objectEvent);
+    if (this.project === undefined) {
+      return;
+    }
+    const project = this.modelBoardService.getHeijunkaBoard().projects.find(aProject => aProject.id === this.project);
+    if (project !== undefined) {
+      const objectEvent = this.modelBoardService.eventFactory.createKanbanCard(this.modelBoardService.currentTopic,
+        project, this.model.name);
+      this.modelBoardService.processObjectEvent(objectEvent);
+      const createdKanbanCard = this.modelBoardService.getHeijunkaBoard().kanbanCards.find(aCard => aCard.id === objectEvent.object);
+      if (createdKanbanCard === undefined) {
+        throw new Error('could not find kanban card just created');
+      } else {
+        // TODO: add to domain: find KanbanCard / Project based on id
+        // TODO: move this functionality to a domain service
+        // TODO: stateModel.states[0] is not the correct way to find initial state ==> move functionality to stateModel
+        const initialState: State = this.modelBoardService.getHeijunkaBoard().stateModel.states[0];
+        const moveToInitialState = this.modelBoardService.eventFactory.moveKanbanCardComplete(this.modelBoardService.currentTopic,
+          createdKanbanCard, initialState);
+        this.modelBoardService.processObjectEvent(moveToInitialState);
+      }
+    } else {
+      throw new Error('could not find project with id ' + this.project);
+    }
   }
 
 }
