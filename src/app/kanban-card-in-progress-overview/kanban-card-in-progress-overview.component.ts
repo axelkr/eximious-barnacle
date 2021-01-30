555387429b0deb820bdec4c832b408e06e377740
Axel <axelkr.dev@google.com>
2021-01-30 07:38:56 +0100

    user can mark task as complete

--- src/app/kanban-card-in-progress-overview/kanban-card-in-progress-overview.component.ts
+++ src/app/kanban-card-in-progress-overview/kanban-card-in-progress-overview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { KanbanCard } from 'outstanding-barnacle';
+import { HeijunkaBoardService } from '../heijunka-board.service';
 
 @Component({
   selector: 'app-kanban-card-in-progress-overview',
@@ -9,9 +10,20 @@ import { KanbanCard } from 'outstanding-barnacle';
 export class KanbanCardInProgressOverviewComponent implements OnInit {
   @Input() kanbanCard: KanbanCard | undefined;
 
-  constructor() { }
+  constructor(private modelBoardService: HeijunkaBoardService) { }
 
   ngOnInit(): void {
   }
 
+  markAsCompleted(): void {
+    if (this.kanbanCard === undefined) {
+      return;
+    }
+
+    const currentState = this.modelBoardService.getHeijunkaBoard().stateModel.getState(
+      this.kanbanCard.history.currentStateTransition().state);
+    const moveToCompleteState = this.modelBoardService.eventFactory.moveKanbanCardComplete(this.modelBoardService.currentTopic,
+      this.kanbanCard, currentState);
+    this.modelBoardService.processObjectEvent(moveToCompleteState);
+  }
 }
