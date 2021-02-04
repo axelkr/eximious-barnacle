31d73475f81b3824b021026d3dbdb39cd86473d9
Axel <axelkr.dev@google.com>
2021-02-04 22:27:13 +0100

    enable rename of projects

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -9,10 +9,31 @@ import { HeijunkaBoardService } from '../heijunka-board.service';
 })
 export class ProjectComponent implements OnInit {
   @Input() project: Project | undefined;
+  renameMode = false;
 
   constructor(public heijunkaBoardService: HeijunkaBoardService) { }
 
   ngOnInit(): void {
   }
 
+  commitRename(event: any): void {
+    this.renameTo(event.target.value);
+    this.renameMode = false;
+  }
+
+  cancelRename(): void {
+    this.renameMode = false;
+  }
+
+  activateRenameMode(): void {
+    this.renameMode = true;
+  }
+
+  private renameTo(newName: string): void {
+    if (this.project === undefined) {
+      return;
+    }
+    const renameEvent = this.heijunkaBoardService.eventFactory.renameProject(this.heijunkaBoardService.currentTopic,this.project,newName);
+    this.heijunkaBoardService.processObjectEvent(renameEvent);
+  }
 }
