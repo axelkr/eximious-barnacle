96c986a44dd5e2ad13d3a85e7d1ec7a0d7a9eb4f
Axel <axelkr.dev@google.com>
2021-02-23 21:21:44 +0100

    simplify stuff

--- src/app/backend/AngularHttpClientFacade.ts
+++ src/app/backend/AngularHttpClientFacade.ts
@@ -1,27 +1,14 @@
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
 import { Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { ObjectEvent } from 'happy-barnacle';
 
-import { IHTTPClient, ObjectEventBackEnd } from 'prime-barnacle/dist/IHTTPClient';
+import { IHTTPClient, ObjectEventBackEnd } from 'prime-barnacle';
 
 export class AngularHttpClientFacade implements IHTTPClient {
     private readonly httpClient: HttpClient;
 
     constructor(httpClient: HttpClient) {
         this.httpClient = httpClient;
-    }
-
-    public static deserializeSingleEvent(json: ObjectEventBackEnd): ObjectEvent {
-        return {
-            topic: json.topic,
-            payload: new Map<string, string>(JSON.parse(json.payload)),
-            time: new Date(json.time),
-            id: json.id,
-            eventType: json.eventType,
-            object: json.object,
-            objectType: json.objectType
-        };
     }
 
     postJson(url: string, json: Record<string, unknown>): void {
