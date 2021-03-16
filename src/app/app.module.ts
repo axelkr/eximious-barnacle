646bd5ac3efa7e2587d2e2326876b115cb6d4be7
Axel <axelkr.dev@google.com>
2021-03-16 22:11:49 +0100

    extract code related to displaying single state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SetupModule } from './setup/setup.module';
+import { ProjectStateDetailsModule } from './project-state-details/project-state-details.module';
 
 import { AppComponent } from './app.component';
 import { AppConfig } from './app.config';
@@ -12,12 +13,7 @@ import { AppConfig } from './app.config';
 import { HeijunkaBoardComponent } from './heijunka-board/heijunka-board.component';
 import { ProjectComponent } from './project/project.component';
 import { ProjectStateComponent } from './project-state/project-state.component';
-import { ProjectStateDetailsComponent } from './project-state-details/project-state-details.component';
 import { KanbanCardCreateComponent } from './kanban-card-create/kanban-card-create.component';
-import { KanbanCardInProgressOverviewComponent } from './kanban-card-in-progress-overview/kanban-card-in-progress-overview.component';
-import { KanbanCardCompleteOverviewComponent } from './kanban-card-complete-overview/kanban-card-complete-overview.component';
-import { ProjectStateKanbanCardsComponent } from './project-state-kanban-cards/project-state-kanban-cards.component';
-import { ProjectStateKanbanCardsByAgeComponent } from './project-state-kanban-cards-by-age/project-state-kanban-cards-by-age.component';
 
 const initializeApp= (appConfig: AppConfig) => () => appConfig.load();
 
@@ -27,19 +23,15 @@ const initializeApp= (appConfig: AppConfig) => () => appConfig.load();
     HeijunkaBoardComponent,
     ProjectComponent,
     ProjectStateComponent,
-    ProjectStateDetailsComponent,
-    KanbanCardCreateComponent,
-    KanbanCardInProgressOverviewComponent,
-    KanbanCardCompleteOverviewComponent,
-    ProjectStateKanbanCardsComponent,
-    ProjectStateKanbanCardsByAgeComponent
+    KanbanCardCreateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    SetupModule
+    SetupModule,
+    ProjectStateDetailsModule
   ],
   providers: [AppConfig,
     { provide: APP_INITIALIZER,
