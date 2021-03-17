24a25cd03584ba52548c9ab34f428096441d4d3e
axelkr <axelkr.dev@gmail.com>
2021-03-17 15:23:20 +0100

    simplify and start looking to update chart based on new data

--- src/app/cumulative-flow-chart/cumulative-flow-chart.component.ts
+++ src/app/cumulative-flow-chart/cumulative-flow-chart.component.ts
@@ -8,6 +8,10 @@ import * as d3Array from 'd3-array';
   styleUrls: ['./cumulative-flow-chart.component.less']
 })
 export class CumulativeFlowChartComponent implements OnInit {
+  private data : number[] =  [4, 8, 15, 16, 23, 42];
+  private readonly data1 = [4, 8, 15, 16, 23, 42];
+  private readonly data2 = [6, 10, 12, 17, 10, 32, 42];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -18,47 +22,45 @@ export class CumulativeFlowChartComponent implements OnInit {
     this.draw();
   }
 
+  public swap(): void {
+    if (this.data === this.data1) {
+      console.log("-> data 2");
+      this.data = this.data2;
+    } else {
+      console.log("-> data 1");
+      this.data = this.data1;
+    }
+  }
+
   public draw(): void {
-    const data = [4, 8, 15, 16, 23, 42];
-    const maxData = d3Array.max(data);
-    if (maxData === undefined ) {
+    const maxData = d3Array.max(this.data);
+    if (maxData === undefined) {
       throw new Error('undefined');
     }
     const width = 420;
     const x = d3.scaleLinear()
-    .domain([0, maxData])
-    .range([0, width]);
+      .domain([0, maxData])
+      .range([0, width]);
 
     const y = d3.scaleBand()
-    .domain(d3.range(data.length).map(aNumber=> ''+aNumber))
-    .range([0, 20 * data.length]);
+      .domain(d3.range(this.data.length).map(aNumber => '' + aNumber))
+      .range([0, 20 * this.data.length]);
 
-    {
-      const svg = d3.select('#renderCFD').append("svg")
-          .attr("width", width)
-          .attr("height", y.range()[1])
-          .attr("font-family", "sans-serif")
-          .attr("font-size", "10")
-          .attr("text-anchor", "end");
-    
-      const bar = svg.selectAll("g")
-        .data(data)
-        .join("g")
-          .attr("transform", (d, i) => `translate(0,${y(''+i)})`);
-    
-      bar.append("rect")
-          .attr("fill", "steelblue")
-          .attr("width", x)
-          .attr("height", y.bandwidth() - 1);
-    
-      bar.append("text")
-          .attr("fill", "white")
-          .attr("x", d => x(d) - 3)
-          .attr("y", (y.bandwidth() - 1) / 2)
-          .attr("dy", "0.35em")
-          .text(d => d);
-    
-      //return svg.node();
-    }
+    const svg = d3.select('#renderCFD').append("svg")
+      .attr("width", width)
+      .attr("height", y.range()[1])
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "10")
+      .attr("text-anchor", "end");
+
+    const bar = svg.selectAll("g")
+      .data(this.data)
+      .join("g")
+      .attr("transform", (d, i) => `translate(0,${y('' + i)})`);
+
+    bar.append("rect")
+      .attr("fill", "steelblue")
+      .attr("width", x)
+      .attr("height", y.bandwidth() - 1);
   }
 }
