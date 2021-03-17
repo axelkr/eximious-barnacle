84b0a8a0c9992da4de011984be0c135ad7392ff2
axelkr <axelkr.dev@gmail.com>
2021-03-17 16:26:34 +0100

    working solution

--- src/app/cumulative-flow-chart/cumulative-flow-chart.component.ts
+++ src/app/cumulative-flow-chart/cumulative-flow-chart.component.ts
@@ -8,59 +8,58 @@ import * as d3Array from 'd3-array';
   styleUrls: ['./cumulative-flow-chart.component.less']
 })
 export class CumulativeFlowChartComponent implements OnInit {
-  private data : number[] =  [4, 8, 15, 16, 23, 42];
+  private data: number[];
   private readonly data1 = [4, 8, 15, 16, 23, 42];
-  private readonly data2 = [6, 10, 12, 17, 10, 32, 42];
+  private readonly data2 = [6, 10, 12, 17, 10, 32];
+  private svg!: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>;
 
-  constructor() { }
+  constructor() {
+    this.data = this.data1;
+  }
 
   ngOnInit(): void {
     this.initChart();
+    this.draw();
   }
 
   private initChart() {
-    this.draw();
+    const width = 300;
+    this.svg = d3.select('#renderCFD').append("svg")
+      .attr("width", width)
+      .attr("height", 20 * this.data.length)
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "10")
+      .attr("text-anchor", "end");
   }
 
   public swap(): void {
     if (this.data === this.data1) {
-      console.log("-> data 2");
       this.data = this.data2;
     } else {
-      console.log("-> data 1");
       this.data = this.data1;
     }
+    this.draw();
   }
 
   public draw(): void {
-    const maxData = d3Array.max(this.data);
-    if (maxData === undefined) {
-      throw new Error('undefined');
-    }
-    const width = 420;
+    const width = 300;
     const x = d3.scaleLinear()
-      .domain([0, maxData])
+      .domain([0, 50])
       .range([0, width]);
-
     const y = d3.scaleBand()
-      .domain(d3.range(this.data.length).map(aNumber => '' + aNumber))
+      .domain(['0','1','2','3','4','5'])
       .range([0, 20 * this.data.length]);
-
-    const svg = d3.select('#renderCFD').append("svg")
-      .attr("width", width)
-      .attr("height", y.range()[1])
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "10")
-      .attr("text-anchor", "end");
-
-    const bar = svg.selectAll("g")
-      .data(this.data)
-      .join("g")
-      .attr("transform", (d, i) => `translate(0,${y('' + i)})`);
-
-    bar.append("rect")
-      .attr("fill", "steelblue")
-      .attr("width", x)
-      .attr("height", y.bandwidth() - 1);
+    this.svg.selectAll("rect")
+      .data(this.data,(d,i)=>''+i)
+      .join(
+        enter => enter.append("rect")
+          .attr("fill", "steelblue")
+          .attr("width", x)
+          .attr("height", y.bandwidth() - 1)
+          .attr("transform", (d, i) => `translate(0,${y('' + i)})`),
+        update => update
+          .attr("width", x),
+        exit => exit.remove()
+      )
   }
 }
