827af170fb58fc6bdad3af04c817a9be02016650
axelkr <axelkr.dev@gmail.com>
2021-03-18 08:09:16 +0100

    extract some code related to one concern

---
+++ src/app/cumulative-flow-chart/BoxModel.ts
@@ -0,0 +1,32 @@
+// simplified box model from CSS
+export class BoxModel {
+    private readonly totalWidth: number;
+    private readonly totalHeight: number;
+    private readonly marginOnEachSide: number;
+
+    constructor(totalWidth: number, totalHeight: number, marginOnEachSide: number) {
+        this.totalWidth = totalWidth;
+        this.totalHeight = totalHeight;
+        this.marginOnEachSide = marginOnEachSide;
+    }
+
+    public contentWidth(): number {
+        return this.totalWidth - 2 * this.marginOnEachSide;
+    }
+
+    public contentHeight(): number {
+        return this.totalHeight - 2 * this.marginOnEachSide;
+    }
+
+    public width(): number {
+        return this.totalWidth;
+    }
+
+    public height(): number {
+        return this.totalHeight;
+    }
+
+    public margin(): number {
+        return this.marginOnEachSide;
+    }
+}
