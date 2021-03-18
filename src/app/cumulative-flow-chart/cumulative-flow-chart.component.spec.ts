167df367e2d9490300845149c8686edfc5778f72
axelkr <axelkr.dev@gmail.com>
2021-03-18 17:42:23 +0100

    fix: inject mock

--- src/app/cumulative-flow-chart/cumulative-flow-chart.component.spec.ts
+++ src/app/cumulative-flow-chart/cumulative-flow-chart.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CumulativeFlowChartComponent } from './cumulative-flow-chart.component';
+import {MockHeijunkaBoardService} from '../heijunka-board.service.spec';
+import {HeijunkaBoardService} from '../heijunka-board.service';
 
 describe('CumulativeFlowChartComponent', () => {
   let component: CumulativeFlowChartComponent;
@@ -8,7 +10,10 @@ describe('CumulativeFlowChartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CumulativeFlowChartComponent ]
+      declarations: [ CumulativeFlowChartComponent ],
+      providers: [
+        { provide: HeijunkaBoardService, useClass: MockHeijunkaBoardService }
+      ]
     })
     .compileComponents();
   });
