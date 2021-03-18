0458826e4fc293ad1c674e9fded07a41369dd78d
axelkr <axelkr.dev@gmail.com>
2021-03-18 08:32:32 +0100

    changes to either end of the series are more pronounced

--- src/app/cumulative-flow-chart/CfdDataGenerator.ts
+++ src/app/cumulative-flow-chart/CfdDataGenerator.ts
@@ -13,8 +13,8 @@ export class CfdDataGenerator {
     constructor() {
         const series1: TimeSeries = [{ date: new Date(2020, 11, 1), value: 4 }, { date: new Date(2020, 11, 2), value: 8 }, { date: new Date(2020, 11, 3), value: 15 },
         { date: new Date(2020, 11, 4), value: 16 }, { date: new Date(2020, 11, 5), value: 23 }, { date: new Date(2020, 11, 6), value: 24 }];
-        const series2: TimeSeries = [{ date: new Date(2020, 11, 1), value: 6 }, { date: new Date(2020, 11, 2), value: 10 }, { date: new Date(2020, 11, 3), value: 12 },
-        { date: new Date(2020, 11, 4), value: 17 }, { date: new Date(2020, 11, 5), value: 10 }, { date: new Date(2020, 11, 6), value: 32 }];
+        const series2: TimeSeries = [{ date: new Date(2020, 11, 1), value: 20 }, { date: new Date(2020, 11, 2), value: 10 }, { date: new Date(2020, 11, 3), value: 12 },
+        { date: new Date(2020, 11, 4), value: 17 }, { date: new Date(2020, 11, 5), value: 10 }, { date: new Date(2020, 11, 6), value: 8 }];
         this.data1 = new Map<State, TimeSeries>()
             .set(this.states[0], series1)
             .set(this.states[1], series2)
