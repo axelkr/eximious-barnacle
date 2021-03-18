ee4169812feb0453e9fd5134c9686caad2b71675
axelkr <axelkr.dev@gmail.com>
2021-03-18 09:20:59 +0100

    document

--- src/app/cumulative-flow-chart/ColorModel.ts
+++ src/app/cumulative-flow-chart/ColorModel.ts
@@ -1,13 +1,14 @@
-import { State,StateModel, LinearizeStateModelService } from "outstanding-barnacle";
+import { State, StateModel, LinearizeStateModelService } from "outstanding-barnacle";
 
+// Stable mapping from a state to colors. Each time, a state should be mapped to the same color.
 export class ColorModel {
-    private readonly allColors: string[] = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'];
+    private readonly allColors: string[] = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'];
 
-    public createColors(stateModel: StateModel): Map<State,string> {
-        const result = new Map<State,string>();
+    public createColors(stateModel: StateModel): Map<State, string> {
+        const result = new Map<State, string>();
         const statesToColor = new LinearizeStateModelService().linearize(stateModel);
-        statesToColor.forEach((aState,index) => {
-            result.set(aState, this.allColors[index% this.allColors.length]); 
+        statesToColor.forEach((aState, index) => {
+            result.set(aState, this.allColors[index % this.allColors.length]);
         })
         return result;
     }
