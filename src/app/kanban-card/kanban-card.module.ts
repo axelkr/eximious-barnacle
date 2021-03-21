f74eb569a1fa472c73949f8c40575e82b69cb004
Axel <axelkr.dev@google.com>
2021-03-21 07:52:04 +0100

    start extracting editing of kanban card's name into separate component

--- src/app/kanban-card/kanban-card.module.ts
+++ src/app/kanban-card/kanban-card.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
+
 import { KanbanCardCreateComponent } from './kanban-card-create/kanban-card-create.component';
 import { KanbanCardOverviewComponent } from './kanban-card-overview/kanban-card-overview.component';
+import { KanbanCardEditNameComponent } from './kanban-card-edit-name/kanban-card-edit-name.component';
 
 @NgModule({
   declarations: [
-    KanbanCardCreateComponent, KanbanCardOverviewComponent
+    KanbanCardCreateComponent, KanbanCardOverviewComponent, KanbanCardEditNameComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   exports: [KanbanCardCreateComponent, KanbanCardOverviewComponent]
 })
