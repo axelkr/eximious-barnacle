b062275b7d85dc0ef2794e964e74951d972c9724
Axel <axelkr.dev@google.com>
2021-03-21 07:19:48 +0100

    simplify: one generic component suffices

--- src/app/kanban-card/kanban-card.module.ts
+++ src/app/kanban-card/kanban-card.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { KanbanCardCreateComponent } from './kanban-card-create/kanban-card-create.component';
-import { KanbanCardCompleteOverviewComponent } from './kanban-card-complete-overview/kanban-card-complete-overview.component';
 import { KanbanCardOverviewComponent } from './kanban-card-overview/kanban-card-overview.component';
-import { KanbanCardInProgressOverviewComponent } from './kanban-card-in-progress-overview/kanban-card-in-progress-overview.component';
 
 @NgModule({
   declarations: [
-    KanbanCardCreateComponent, KanbanCardCompleteOverviewComponent, KanbanCardInProgressOverviewComponent, KanbanCardOverviewComponent
+    KanbanCardCreateComponent, KanbanCardOverviewComponent
   ],
   imports: [
     CommonModule,
     FormsModule
   ],
-  exports: [KanbanCardCreateComponent, KanbanCardCompleteOverviewComponent,
-    KanbanCardInProgressOverviewComponent, KanbanCardOverviewComponent]
+  exports: [KanbanCardCreateComponent, KanbanCardOverviewComponent]
 })
 export class KanbanCardModule { }
