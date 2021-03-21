dc74cf28a41ca2098570d768eb1136c17386b901
Axel <axelkr.dev@google.com>
2021-03-21 13:23:12 +0100

    simplify

--- src/app/cumulative-flow-chart/CumulativeFlowChart.ts
+++ src/app/cumulative-flow-chart/CumulativeFlowChart.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import * as d3 from 'd3';
-import { StateModel } from 'outstanding-barnacle';
+import { State, StateModel } from 'outstanding-barnacle';
 
 import { BoxModel } from './BoxModel';
 import { StateTimeSeries } from './CfdDataGenerator';
@@ -9,14 +9,13 @@ import { ColorModel } from './ColorModel';
 
 export class CumulativeFlowChart {
     private readonly chartBox = new BoxModel(200 * 1.618, 200, 30, 20);
-    private readonly colorModel = new ColorModel();
-    private readonly stateModel: StateModel;
+    private readonly colorsPerState: Map<State, string>;
     private svg!: d3.Selection<SVGGElement, unknown, HTMLElement, any>;
     private xAxis!: d3.Selection<SVGGElement, unknown, HTMLElement, any>;
     private yAxis!: d3.Selection<SVGGElement, unknown, HTMLElement, any>;
 
     constructor(stateModel: StateModel) {
-        this.stateModel = stateModel;
+        this.colorsPerState = new ColorModel().createColors(stateModel);
     }
 
     public init(id: string) {
@@ -44,8 +43,8 @@ export class CumulativeFlowChart {
                 );
             return;
         }
+
         const stacked = new StackingService().convertToStack(completeData);
-        const colorOfData = this.colorModel.createColors(this.stateModel);
 
         const x = d3.scaleTime<number>()
             .domain(d3.extent(completeData[0].entries, d => d.date) as [Date, Date])
@@ -64,7 +63,7 @@ export class CumulativeFlowChart {
             .data(stacked)
             .join(
                 enter => enter.append('path')
-                    .style('fill', (_, index) => colorOfData.get(completeData[index].state) as string),
+                    .style('fill', (_, index) => this.colorsPerState.get(completeData[index].state) as string),
                 update => update,
                 exit => exit.remove()
             )
