8679975f2eca743ea0df755919f4b53e1d75dba3
Axel <axelkr.dev@google.com>
2021-03-24 05:54:55 +0100

    fix test

--- src/app/kanban-card/kanban-card.service.spec.ts
+++ src/app/kanban-card/kanban-card.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MockObjectStoreBackendService } from '../backend/object-store-backend.service.spec';
+import { ObjectStoreBackendService } from '../backend/object-store-backend.service';
 
+import { HeijunkaBoardService } from '../heijunka-board.service';
 import { KanbanCardService } from './kanban-card.service';
+import { MockHeijunkaBoardService } from '../heijunka-board.service.spec';
 
 describe('KanbanCardService', () => {
   let service: KanbanCardService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: ObjectStoreBackendService, useClass: MockObjectStoreBackendService },
+        { provide: HeijunkaBoardService, useClass: MockHeijunkaBoardService }
+      ]
+    });
     service = TestBed.inject(KanbanCardService);
   });
 
