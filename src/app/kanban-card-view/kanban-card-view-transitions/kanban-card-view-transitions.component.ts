179854770abe4dacc76ea4bf78bf92e1908104a0
Axel <axelkr.dev@google.com>
2021-03-24 05:38:23 +0100

    available transitions depend on current transition

--- src/app/kanban-card-view/kanban-card-view-transitions/kanban-card-view-transitions.component.ts
+++ src/app/kanban-card-view/kanban-card-view-transitions/kanban-card-view-transitions.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { KanbanCard, State } from 'outstanding-barnacle';
+import { KanbanCard, State, TransitionType } from 'outstanding-barnacle';
 import { HeijunkaBoardService } from '../../heijunka-board.service';
 import { KanbanCardService } from '../../kanban-card/kanban-card.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-kanban-card-view-transitions',
@@ -11,10 +10,26 @@ import { Router } from '@angular/router';
 export class KanbanCardViewTransitionsComponent implements OnInit {
   @Input() kanbanCard: KanbanCard | undefined;
   @Input() state: State | undefined;
+  transitionType = TransitionType;
+  transition: TransitionType | undefined;
 
-  constructor(private router: Router, public boardService: HeijunkaBoardService, public kanbanCardService: KanbanCardService) { }
+  constructor(public boardService: HeijunkaBoardService, public kanbanCardService: KanbanCardService) { }
 
   ngOnInit(): void {
+    this.readTransitionTypeOfKanbanCard();
   }
 
+  private readTransitionTypeOfKanbanCard() {
+    this.transition = undefined;
+    if (this.kanbanCard === undefined) {
+      return;
+    }
+
+    const currentTransition = this.kanbanCard.history.currentStateTransition();
+    if (currentTransition === undefined) {
+      return;
+    }
+    console.log(currentTransition.type);
+    this.transition = currentTransition.type;
+  }
 }
