b7bce2e718992f21473e5b2b98d0fb33bc447b4d
axelkr <axelkr.dev@gmail.com>
2021-03-26 11:40:56 +0100

    simplify

--- src/app/context/context.module.ts
+++ src/app/context/context.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { ContextCreateComponent } from './context-create/context-create.component';
-import { ContextListComponent } from './context-list/context-list.component';
 import { ContextSelectComponent } from './context-select/context-select.component';
 
 @NgModule({
-  declarations: [ContextCreateComponent, ContextListComponent, ContextSelectComponent],
+  declarations: [ContextCreateComponent, ContextSelectComponent],
   imports: [
     CommonModule, FormsModule
   ],
-  exports: [ContextCreateComponent, ContextListComponent, ContextSelectComponent]
+  exports: [ContextCreateComponent, ContextSelectComponent]
 })
 export class ContextModule { }
