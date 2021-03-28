3b65d1994c984d0c4c2ebea0f3f33b007e450d38
Axel <axelkr.dev@google.com>
2021-03-28 14:21:35 +0200

    and remove unused reference

--- src/app/setup-view/project-add/project-add.component.ts
+++ src/app/setup-view/project-add/project-add.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HeijunkaBoardService } from '../../domain-services/heijunka-board.service';
 import { StateModel } from 'outstanding-barnacle';
 import { ProjectService } from '../../domain-services/project.service';
 import { StateModelService } from '../../domain-services/state-model.service';
@@ -12,8 +11,7 @@ import { StateModelService } from '../../domain-services/state-model.service';
 export class ProjectAddComponent implements OnInit {
   model: { name: string; stateModel: StateModel | undefined } = { name: '', stateModel: undefined };
 
-  constructor(public modelBoardService: HeijunkaBoardService, private projectService: ProjectService,
-    public stateModelService: StateModelService) {
+  constructor(private projectService: ProjectService, public stateModelService: StateModelService) {
   }
 
   ngOnInit(): void {
