ab761e71632d929043244308cb4e612c76ec9f90
Axel <axelkr.dev@google.com>
2021-03-28 14:19:29 +0200

    remove dead code & only display what doesn't rely on a topic being displayed

--- src/app/setup-view/setup-view.component.ts
+++ src/app/setup-view/setup-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TopicService } from '../domain-services/topic.service';
 
 @Component({
   selector: 'app-setup-view',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetupViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(public topicService: TopicService) { }
 
   ngOnInit(): void {
   }
