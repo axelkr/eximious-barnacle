cd72f751bf4007ef4f078c696bc5e0d90eb5d151
Axel <axelkr.dev@google.com>
2021-03-28 12:53:29 +0200

    avoid exception during startup, also: used service matches description in template

--- src/app/heijunka-board-view/heijunka-board-view.component.ts
+++ src/app/heijunka-board-view/heijunka-board-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeijunkaBoardService } from '../domain-services/heijunka-board.service';
 import { ProjectService } from '../domain-services/project.service';
+import { TopicService } from '../domain-services/topic.service';
 
 @Component({
   selector: 'app-heijunka-board-view',
@@ -8,7 +9,7 @@ import { ProjectService } from '../domain-services/project.service';
   styleUrls: ['./heijunka-board-view.component.less']
 })
 export class HeijunkaBoardViewComponent implements OnInit {
-  constructor(public heijunkaBoardService: HeijunkaBoardService, public projectService: ProjectService) {
+  constructor(public heijunkaBoardService: HeijunkaBoardService, public projectService: ProjectService, public topicService: TopicService) {
   }
 
   ngOnInit(): void {
