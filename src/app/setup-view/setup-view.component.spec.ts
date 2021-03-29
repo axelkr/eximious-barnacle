01794160223a6940dfabc6f075490969134bad98
Axel <axelkr.dev@google.com>
2021-03-29 21:28:30 +0200

    fix: inject missing objects

--- src/app/setup-view/setup-view.component.spec.ts
+++ src/app/setup-view/setup-view.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MockObjectStoreBackendService } from '../backend/object-store-backend.service.spec';
+import { ObjectStoreBackendService } from '../backend/object-store-backend.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SetupViewComponent } from './setup-view.component';
+import { MockHeijunkaBoardService } from '../domain-services/heijunka-board.service.spec';
+import { HeijunkaBoardService } from '../domain-services/heijunka-board.service';
 
 describe('SetupViewComponent', () => {
   let component: SetupViewComponent;
@@ -8,9 +14,16 @@ describe('SetupViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SetupViewComponent ]
+      declarations: [SetupViewComponent],
+      imports: [
+        RouterTestingModule, HttpClientTestingModule
+      ],
+      providers: [
+        { provide: HeijunkaBoardService, useClass: MockHeijunkaBoardService },
+        { provide: ObjectStoreBackendService, useClass: MockObjectStoreBackendService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
