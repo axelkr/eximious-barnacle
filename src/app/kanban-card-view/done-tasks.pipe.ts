b31e619396d8a51964f18489da405699222dbbf0
Axel <axelkr.dev@google.com>
2021-03-30 05:32:59 +0200

    user may choose what to filter for

--- src/app/kanban-card-view/done-tasks.pipe.ts
+++ src/app/kanban-card-view/done-tasks.pipe.ts
@@ -6,8 +6,12 @@ import { Task } from 'outstanding-barnacle';
 })
 export class DoneTasksPipe implements PipeTransform {
 
-  transform(tasks: Task[], ...args: unknown[]): Task[] {
-    return tasks.filter(a => !a.isDone());
-  }
+  transform(tasks: Task[], filterDoneTasks: boolean = true): Task[] {
 
+    if (filterDoneTasks) {
+      return tasks.filter(a => a.isDone());
+    } else {
+      return tasks.filter(a => !a.isDone());
+    }
+  }
 }
