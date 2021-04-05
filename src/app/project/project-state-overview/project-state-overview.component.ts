e4009d3b8c9a976f0c2e83b79e767661534fb829
Axel <axelkr.dev@google.com>
2021-04-05 09:22:21 +0200

    reuse input color

--- src/app/project/project-state-overview/project-state-overview.component.ts
+++ src/app/project/project-state-overview/project-state-overview.component.ts
@@ -1,33 +1,24 @@
-import { Component, OnInit, Input, AfterContentInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { ProjectService } from '../../domain-services/project.service';
 import { Project, State, TransitionType } from 'outstanding-barnacle';
 import { KanbanCardService } from '../../domain-services/kanban-card.service';
-import { ColorizeStateModelService } from '../../state-model/colorize-state-model.service';
 
 @Component({
   selector: 'app-project-state-overview',
   templateUrl: './project-state-overview.component.html',
   styleUrls: ['./project-state-overview.component.less']
 })
-export class ProjectStateOverviewComponent implements OnInit, AfterContentInit {
+export class ProjectStateOverviewComponent implements OnInit {
   @Input() project: Project | undefined;
   @Input() state: State | undefined;
   @Input() color: string | undefined;
-  myColor: string | undefined;
 
   transitionType = TransitionType;
 
   constructor(private router: Router, public projectService: ProjectService,
-    public kanbanCardService: KanbanCardService, public colorizeStateModelService: ColorizeStateModelService) { }
-
-  ngAfterContentInit(): void {
-    if (this.project === undefined || this.state === undefined) {
-      return;
-    }
-    this.myColor = this.colorizeStateModelService.createColors(this.projectService.getStateModel(this.project)).get(this.state);
-  }
+    public kanbanCardService: KanbanCardService) { }
 
   ngOnInit(): void {
   }
