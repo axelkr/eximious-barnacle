724bf0cb0a5300e525f8cf2dea947f175657e52d
Axel <axelkr.dev@google.com>
2021-05-22 07:18:12 +0200

    add title to setup view as well: Every top-level view now has a title

---
+++ src/app/setup-view/setup-view-title/setup-view-title.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setup-view-title',
+  templateUrl: './setup-view-title.component.html'
+})
+export class SetupViewTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/setup-view/setup-view.module.ts
+++ src/app/setup-view/setup-view.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { SetupViewRoutingModule } from './setup-view-routing.module';
+import { MenuModule } from '../menu/menu.module';
 
 import { SetupViewComponent } from './setup-view.component';
 import { StateModelsComponent } from './state-models/state-models.component';
@@ -10,12 +11,13 @@ import { TopicCreateComponent } from './topic-create/topic-create.component';
 import { TopicSelectComponent } from './topic-select/topic-select.component';
 import { ProjectAddComponent } from './project-add/project-add.component';
 import { ContextCreateComponent } from './context-create/context-create.component';
+import { SetupViewTitleComponent } from './setup-view-title/setup-view-title.component';
 
 @NgModule({
   declarations: [StateModelsComponent, TopicCreateComponent, TopicSelectComponent,
-    ProjectAddComponent, SetupViewComponent, ContextCreateComponent],
+    ProjectAddComponent, SetupViewComponent, ContextCreateComponent, SetupViewTitleComponent],
   imports: [
-    CommonModule, FormsModule, SetupViewRoutingModule
+    CommonModule, FormsModule, SetupViewRoutingModule, MenuModule
   ],
   exports: [SetupViewComponent]
 })
