8970fff8bdbc2ff7f297a6e4e43d8adb70143066
Axel <axelkr.dev@google.com>
2021-06-12 09:20:17 +0200

    states not in focus are muted

--- src/app/state-model/colorize-state-model.service.ts
+++ src/app/state-model/colorize-state-model.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { State, StateModel, LinearizeStateModelService } from 'outstanding-barnacle';
+import { StateModelService } from '../domain-services/state-model.service';
 
 export type StateToColor = Map<State, string>;
 
@@ -10,13 +11,15 @@ export type StateToColor = Map<State, string>;
 export class ColorizeStateModelService {
   private readonly allColors: string[] = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#999999'];
 
-  constructor() { }
+  constructor(private stateModelService: StateModelService) { }
 
   public createColors(stateModel: StateModel): StateToColor {
     const result = new Map<State, string>();
+    const grey = '#cccccc';
     const statesToColor = new LinearizeStateModelService().linearize(stateModel);
     statesToColor.forEach((aState, index) => {
-      result.set(aState, this.allColors[index % this.allColors.length]);
+      const color = this.stateModelService.isInFocus(aState) ? this.allColors[index % this.allColors.length] : grey;
+      result.set(aState, color);
     });
     return result;
   }
