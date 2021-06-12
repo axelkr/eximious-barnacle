01b7e1618de62eb5b78c363b22c2232ec88f55d9
Axel <axelkr.dev@google.com>
2021-06-12 16:38:08 +0200

    setup: progress during period

---
+++ src/app/kanban-card/kanban-card-progress-list/kanban-card-progress-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-card-progress-list',
+  templateUrl: './kanban-card-progress-list.component.html'
+})
+export class KanbanCardProgressListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/kanban-card/kanban-card.module.ts
+++ src/app/kanban-card/kanban-card.module.ts
@@ -5,16 +5,17 @@ import { RouterModule } from '@angular/router';
 
 import { KanbanCardCreateComponent } from './kanban-card-create/kanban-card-create.component';
 import { KanbanCardOverviewComponent } from './kanban-card-overview/kanban-card-overview.component';
+import { KanbanCardProgressListComponent } from './kanban-card-progress-list/kanban-card-progress-list.component';
 
 @NgModule({
   declarations: [
-    KanbanCardCreateComponent, KanbanCardOverviewComponent
+    KanbanCardCreateComponent, KanbanCardOverviewComponent, KanbanCardProgressListComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule
   ],
-  exports: [KanbanCardCreateComponent, KanbanCardOverviewComponent]
+  exports: [KanbanCardCreateComponent, KanbanCardOverviewComponent, KanbanCardProgressListComponent]
 })
 export class KanbanCardModule { }
