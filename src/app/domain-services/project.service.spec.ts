6931cab866ee68cb8b664ee6da3f6b735c81bedb
Axel <axelkr.dev@google.com>
2021-08-27 21:59:17 +0200

    more tests

--- src/app/domain-services/project.service.spec.ts
+++ src/app/domain-services/project.service.spec.ts
@@ -1,2 +1,13 @@
+import { Project } from 'outstanding-barnacle';
+
 export class MockProjectService {
+    public returnValueIsInFocus: boolean = true;
+
+    public availableProjects(): Project[] {
+        return [];
+    }
+
+    public isInFocus(project: Project | string): boolean {
+        return this.returnValueIsInFocus;
+    }
 }
--- src/app/kanban-card/kanban-card-in-focus.pipe.spec.ts
+++ src/app/kanban-card/kanban-card-in-focus.pipe.spec.ts
@@ -4,6 +4,7 @@ import { MockProjectService } from '../domain-services/project.service.spec';
 import { MockStateModelService } from '../domain-services/state-model.service.spec';
 import { StateModelService } from '../domain-services/state-model.service';
 import { ProjectService } from '../domain-services/project.service';
+import { KanbanCard } from 'outstanding-barnacle';
 
 describe('KanbanCardInFocusPipe', () => {
   let pipe: KanbanCardInFocusPipe;
@@ -23,11 +24,21 @@ describe('KanbanCardInFocusPipe', () => {
   });
 
 
-  it('create an instance', () => {
-    expect(pipe).toBeTruthy();
+  it('can process empty array', () => {
+    expect(pipe.transform([])).toHaveSize(0);
   });
-  // test: no date given => all cards preserved
-  // test: first date given => last transition before that is discarded
+
+  it('all cards are preserved if no date is given', () => {
+    const someKanbanCards: KanbanCard[] = [KanbanCard.create('id', 'project'), KanbanCard.create('id2', 'project')];
+    expect(pipe.transform(someKanbanCards)).toHaveSize(2);
+  });
+
+  it('kanban card without transition can be handled', () => {
+    const oneKanbanCard: KanbanCard[] = [KanbanCard.create('id', 'project')];
+    expect(pipe.transform(oneKanbanCard)).toHaveSize(1);
+  });
+
+  // test: first date given => card is kept if no transition available or if last transition after first date
   // test: first & last date given => card preserved if at least one transition falls into that period
   // test: border case: transition at first date is preserved
   // test: border case: transition at last date is preserved
